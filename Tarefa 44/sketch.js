affeabb42dd8b5e1ecbd361308f7caf65d65de6e
BICSonic <tubecorrea@gmail.com>
2026-07-10 13:48:16 -0300

    Add files via upload

---
+++ Tarefa 44/sketch.js
@@ -0,0 +1,11 @@
+var background;
+var coin;
+var cube; 
+var flag
+
+function preload() {
+  background = loadImage("background.png");
+  coin = loadImage("coin.png");
+  cube = loadImage("cube.png");
+  flag = loadImage("flag.png");
+}
